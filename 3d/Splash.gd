2ad07f5ce50511c246d386bb6e8636add67da0ea
John Gabriel <accounts@johngabriel.co.uk>
2023-01-22 12:02:02 +0000

    Implement skipping splash screen

--- 3d/Splash.gd
+++ 3d/Splash.gd
@@ -2,9 +2,16 @@ extends Spatial
 
 onready var anim_player : AnimationPlayer = $AnimationPlayer
 
-func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
+func goto_title() -> void:
 	Global.color_rect.show()
 	get_tree().change_scene("res://scenes/Menu.tscn")
+
+func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
+	goto_title()
+
+func _input(event : InputEvent) -> void:
+	if event.is_action_pressed("next_line"):
+		goto_title()
 
 func _ready() -> void:
 	Global.color_rect.hide()
