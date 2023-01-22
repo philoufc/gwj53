24d3374bf68d52e93d02e5b99ac87f097b4f7d5e
philou <117863579+philoufc@users.noreply.github.com>
2023-01-22 16:57:47 -0500

    out with the smoke

--- scenes/Cauldron.gd
+++ scenes/Cauldron.gd
@@ -1,20 +1,10 @@
 extends Sprite
 
-onready var animated_sprite = $AnimatedSprite
+#onready var animated_sprite = $AnimatedSprite
 onready var blblblu = preload("res://audio/blblblu.ogg")
 
 func _ready() -> void:
-	animated_sprite.stop()
-	animated_sprite.animation = "smoked"
-	animated_sprite.frame = 0
-	if OS.has_feature("HTML5"):
-		animated_sprite.queue_free()
+	pass
 
 func smoking_is_bad():
-	if OS.has_feature("HTML5"):
-		return
-	else:
-		yield(get_tree(), "idle_frame")
-		Global.sfx_player.stream = blblblu
-		Global.sfx_player.play()
-		animated_sprite.play("smoked")
+	pass
