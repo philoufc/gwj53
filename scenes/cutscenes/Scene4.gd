20c262da7f21f8a4d4028dc883e5561419644fd4
fbcosentino <fbcosentino>
2023-01-22 19:29:58 +0000

    fix levels going back to level select in story mode

--- scenes/cutscenes/Scene4.gd
+++ scenes/cutscenes/Scene4.gd
@@ -3,7 +3,7 @@ extends Control
 onready var convo_scene : Control = $ConvoScene
 
 func _on_ConvoScene_scene_done() -> void:
-	Global.play_level(7, true)
+	Global.play_level(7, false)
 
 func _ready() -> void:
 	convo_scene.play_scene("scene4")
--- scenes/cutscenes/Scene6.gd
+++ scenes/cutscenes/Scene6.gd
@@ -3,7 +3,7 @@ extends Control
 onready var convo_scene : Control = $ConvoScene
 
 func _on_ConvoScene_scene_done() -> void:
-	Global.play_level(10, true)
+	Global.play_level(10, false)
 
 func _ready() -> void:
 	convo_scene.play_scene("scene6")
